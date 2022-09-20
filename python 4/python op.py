1cb09e3762d48b0893de6cb9dccee07e97883eb9
NuthalapatiDinesh <112842345+NuthalapatiDinesh@users.noreply.github.com>
2022-09-20 10:47:14 +0530

    Add files via upload

---
+++ python 4/python op.py
@@ -0,0 +1,22 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 4.py =================
+enter the word121
+yes
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 4.py =================
+enter the word-121
+no
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 4.py =================
+enter the word10
+no
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 4.py =================
+enter the wordabc
+no
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 4.py =================
+enter the word0
+yes
