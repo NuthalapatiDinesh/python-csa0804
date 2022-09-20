0ce5c240070e1d52962b9f7c061217507303d548
NuthalapatiDinesh <112842345+NuthalapatiDinesh@users.noreply.github.com>
2022-09-20 14:01:08 +0530

    Add files via upload

---
+++ python 12/python op.py
@@ -0,0 +1,22 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/Rahul/Desktop/python 12.py =================
+enter the value:19.47
+not a valid input
+>>> 
+================= RESTART: C:/Users/Rahul/Desktop/python 12.py =================
+enter the value:19.37
+not a valid input
+>>> 
+================= RESTART: C:/Users/Rahul/Desktop/python 12.py =================
+enter the value:0
+not a valid input
+>>> 
+================= RESTART: C:/Users/Rahul/Desktop/python 12.py =================
+enter the value:2000
+it is a leap year
+>>> 
+================= RESTART: C:/Users/Rahul/Desktop/python 12.py =================
+enter the value:-1428
+not a valid input
