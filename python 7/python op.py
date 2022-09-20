93407d1c2caf8f1b0edc9acd2601b55eccc845e2
NuthalapatiDinesh <112842345+NuthalapatiDinesh@users.noreply.github.com>
2022-09-20 11:01:28 +0530

    Add files via upload

---
+++ python 7/python op.py
@@ -0,0 +1,18 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 7.py =================
+enter input1
+5
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 7.py =================
+enter input2
+15
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 7.py =================
+enter input33
+66045
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 7.py =================
+enter input10
+1001
