cb9caad0ab5463f12fa1bda793472bb999cf3f2c
NuthalapatiDinesh <112842345+NuthalapatiDinesh@users.noreply.github.com>
2022-09-20 10:51:47 +0530

    Add files via upload

---
+++ python 5/python op.py
@@ -0,0 +1,42 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 5.py =================
+Enter the Number of new loaves purchased : 4
+Enter the Number of old loaves purchased : 6
+Regular Price : Rs. 185
+Old loaves Amount : Rs.  444.0
+New loaves Amount : Rs.  740
+Total Amount : Rs.  1184.0
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 5.py =================
+Enter the Number of new loaves purchased : -1
+Enter the Number of old loaves purchased : 5
+Regular Price : Rs. 185
+Old loaves Amount : Rs.  370.0
+New loaves Amount : Rs.  -185
+Total Amount : Rs.  185.0
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 5.py =================
+Enter the Number of new loaves purchased : 0
+Enter the Number of old loaves purchased : 6
+Regular Price : Rs. 185
+Old loaves Amount : Rs.  444.0
+New loaves Amount : Rs.  0
+Total Amount : Rs.  444.0
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 5.py =================
+Enter the Number of new loaves purchased : 7
+Enter the Number of old loaves purchased : 8
+Regular Price : Rs. 185
+Old loaves Amount : Rs.  592.0
+New loaves Amount : Rs.  1295
+Total Amount : Rs.  1887.0
+>>> 
+================== RESTART: C:/Users/Rahul/Desktop/python 5.py =================
+Enter the Number of new loaves purchased : 3
+Enter the Number of old loaves purchased : 4
+Regular Price : Rs. 185
+Old loaves Amount : Rs.  296.0
+New loaves Amount : Rs.  555
+Total Amount : Rs.  851.0
